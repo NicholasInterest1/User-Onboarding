96f7571ad7850bf09ba049117dcf1c8c65a716a0
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-08-22 23:26:26 -0400

    final commit

--- on-boarding/src/components/Form.js
+++ on-boarding/src/components/Form.js
@@ -13,34 +13,34 @@ const PatsRoster = ({ errors, touched, values, status }) => {
 
   return (
     <div className="patriots-form">
-      <h1>Patriots Roster</h1>
       <Form>
-        <Field type="text" name="playername" placeholder="Player Name" />
-        {touched.playername && errors.playername && (
-          <p className="error">{errors.playername}</p>
+        <Field type="text" name="name" placeholder="Player Name" />
+        {touched.name && errors.name && <p className="error">{errors.name}</p>}
+
+        <Field type="text" name="email" placeholder="email" />
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
         )}
 
-        <Field type="text" name="Jersey Number" placeholder="Jersey Number" />
-        {touched.JerseyNumber && errors.JerseyNumber && <p className="error">{errors.JerseyNumber}</p>}
+        <Field type="password" name="password" placeholder="password" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
 
-        <Field component="select" className="value-select" name="name">
+        <Field component="select" className="value-select" name="choose">
           <option>Please Choose an Option</option>
-          <option value="qb">Quarterback</option>
-          <option value="rb">Running Back</option>
-          <option value="wr">Wide Receiver</option>
-          <option value="te">Tight End</option>
-          <option value="cb">Corner Back</option>
-          <option value="fs">Free Safety</option>
-          <option value="ss">Strong Safety</option>
+          <option value="QB">Quarterback</option>
+          <option value="RB">Running Back</option>
+          <option value="WR">Wide Receiver</option>
+          <option value="TE">Tight End</option>
+          <option value="CB">Corner Back</option>
+          <option value="FS">Free Safety</option>
+          <option value="SS">Strong Safety</option>
         </Field>
 
         <label className="checkbox-container">
-          terms of Service
-          <Field
-            type="checkbox"
-            name="terms of Service"
-            checked={values.termsofservice}
-          />
+          Terms of Service
+          <Field type="checkbox" name="tos" checked={values.tos} />
           <span className="checkmark" />
         </label>
 
@@ -48,7 +48,7 @@ const PatsRoster = ({ errors, touched, values, status }) => {
           component="textarea"
           type="text"
           name="playerNotes"
-          placeholder="PlayerNotes"
+          placeholder="playerNotes"
         />
         {touched.playerNotes && errors.playerNotes && (
           <p className="error">{errors.playerNotes}</p>
@@ -56,33 +56,39 @@ const PatsRoster = ({ errors, touched, values, status }) => {
 
         <button type="submit">Submit!</button>
       </Form>
-
-      {patriots.map(patriots => (
-        <ul key={patriots.id}>
-          <li>Player Name: {patriots.playername}</li>
-          <li>Jersey Number: {patriots.JerseyNumber}</li>
-          <li>name: {patriots.name}</li>
-        </ul>
-      ))}
+      <div className="complete-form">
+        {patriots.map(patriot => (
+          <ul key={patriot.id}>
+              <div className="p-class">
+            <p>Player Name: {patriot.name}</p>
+            <p>Email: {patriot.email}</p>
+            <p>Password: {patriot.password}</p>
+            <p>Position: {patriot.choose}</p>
+            <p>Player Notes: {patriot.playerNotes}</p>
+            </div>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 };
 
 const FormikPatsRoster = withFormik({
-  mapPropsToValues({ playername, JerseyNumber, playerNotes, name, termsofservice }) {
+  mapPropsToValues({ name, email, playerNotes, tos, password }) {
     return {
-      termsofservice: termsofservice || false,
+      tos: tos || false,
       name: name || "",
-      playername: playername || "",
-      JerseyNumber: JerseyNumber || "{number}",
+      password: password || "",
+      email: email || "",
       playerNotes: playerNotes || ""
     };
   },
 
   validationSchema: Yup.object().shape({
-    playername: Yup.string().required("You shall not pass, without this field!!"),
-    jerseynumber: Yup.string(),
-    playerNotes: Yup.string()
+    name: Yup.string().required("You shall not pass, without this field!!"),
+    email: Yup.string(),
+    playerNotes: Yup.string(),
+    password: Yup.string()
   }),
 
   handleSubmit(values, { setStatus }) {
